b0eaf8dc88fd145014562e67185d8cd4e702f7e7
sftvov <SUrabota@mail.ru>
2021-05-07 19:03:53 +0300

    90%

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,8 @@
 @@include('webp.js');
+
+let burger = document.querySelector('.header__burger');
+
+burger.onclick = function () {
+	burger.classList.toggle('active');
+	document.querySelector('.header__menu-wraper').classList.toggle('active');
+};
