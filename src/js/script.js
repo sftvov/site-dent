cce72853d2202b22394a014b5fb0bc12fea3fc7c
sftvov <63662363+sftvov@users.noreply.github.com>
2021-07-19 21:29:07 +0300

    start spollers

--- src/js/script.js
+++ src/js/script.js
@@ -17,6 +17,30 @@ spaceForHeader();
 @@include('webp.js');
 @@include('burger.js');
 @@include('tabs.js');
+// ----------------------------------------------------------------------
+
+// const spollersArray = document.querySelectorAll('[data-spollers');
+// if (spollersArray.length > 0) {
+// 	const spollersRegular = Array.from(spollersArray).filter((item,index,self) => {
+// 		return !item.dataset.spollers.split(',')[0];
+// 	});
+// 	if (spollersRegular.length > 0) {
+// 		initSpollers(spollersRegular);
+// 	}
+// }
+
+const spollersRegular = document.querySelectorAll('[data-spollers]');
+if (spollersRegular.length > 0) {
+	initSpollers(spollersRegular);
+}
+
+function initSpollers(spollersArray) {
+	spollersArray.forEach(spollersBlock => {
+		spollersBlock.classList.add('_init');
+		initSpollerBody(spollersBlock);
+		spollersBlock.addEventListener('click', setSpollerAction);
+	})
+}
 
 // ----------------------------------------------------------------------
 
