c952402744b4d3d7967551ed0679b58f50d05414
sftvov <SUrabota@mail.ru>
2021-09-14 19:10:20 +0300

    more photos

--- gulpfile.js
+++ gulpfile.js
@@ -14,14 +14,14 @@ let path = {
 		html: [source_folder + '/*.html', '!' + source_folder + '/_*.html'],
 		css: source_folder + '/scss/style.scss',
 		js: source_folder + '/js/script.js',
-		img: source_folder + '/img/**/*.{jpg,png,svg,gif,ico,webp}',
+		img: source_folder + '/img/**/*.{jpg,jpeg,png,svg,gif,ico,webp}',
 		fonts: source_folder + '/fonts/*.ttf',
 	},
 	watch: {
 		html: source_folder + '/**/*.html',
 		css: source_folder + '/scss/**/*.scss',
 		js: source_folder + '/js/**/*.js',
-		img: source_folder + '/img/**/*.{jpg,png,svg,gif,ico,webp}',
+		img: source_folder + '/img/**/*.{jpg,jpeg,png,svg,gif,ico,webp}',
 	},
 	clean: './' + project_folder + '/',
 };
@@ -164,10 +164,8 @@ function clean() {
 }
 
 let build = gulp.series(
-	// clean,
-	gulp.parallel(js, css, html, images, fonts),
-	fontsStyle
-);
+	// clean, 
+	gulp.parallel(js, css, html, images, fonts), fontsStyle);
 let watch = gulp.parallel(build, watchFiles, browserSync);
 
 exports.clean = clean;
